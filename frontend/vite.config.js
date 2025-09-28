b22a3814da2a3f88e837a46e5fd16ced6956c339
Farida964 <imfaravi@gmail.com>
2025-09-28 20:46:29 +0700

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'frontend',        // folder utama project
+  build: {
+    outDir: '../dist',     // hasil build keluar di root/dist
+    emptyOutDir: true
+  }
 })
