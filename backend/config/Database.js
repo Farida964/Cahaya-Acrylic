4e0d50706902ed8b08be8390d974891c87d4f0c4
Royhan67102 <royhanaudy14@gmail.com>
2025-10-06 12:52:42 +0700

    laL

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("cahaya_db", "root", "Farida(90)", {
+const db = new Sequelize("cahaya_db", "root", "FaridaRV(90)", {
   host: "127.0.0.1",
   dialect: "mysql",
   logging: false,
